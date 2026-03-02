6cde683e3470e9daeb0b95dddebde111e4c44146
jcarrillo0327 <joshua.carrillo0327@gmail.com>
2026-03-02 09:53:21 -0600

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,26 @@
+let pressureData = [];
+let cakeData = [];
 
+let pressureCtx = document.getElementById("pressureChart").getContext("2d");
+let cakeCtx = document.getElementById("cakeChart").getContext("2d");
+
+let pressureChart = new Chart(pressureCtx, {
+    type: "line",
+    data: { labels: [], datasets: [{ label: "Pressure Drop (psi)", data: [] }] }
+});
+
+let cakeChart = new Chart(cakeCtx, {
+    type: "line",
+    data: { labels: [], datasets: [{ label: "Cake Thickness (inches)", data: [] }] }
+});
+
+function updateCharts(data) {
+    pressureChart.data.labels.push("");
+    pressureChart.data.datasets[0].data.push(data.pressure);
+
+    cakeChart.data.labels.push("");
+    cakeChart.data.datasets[0].data.push(data.cake);
+
+    pressureChart.update();
+    cakeChart.update();
+}
